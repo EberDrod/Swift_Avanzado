40cac4e807b40eef600d77a7e761912e1c186c03
Eb Peña <eberg.pena@gmail.com>
2020-12-29 17:52:06 -0600

    Se ha asociado playground

---
+++ Asociatividad.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Prescedencia y Asociatividad
+
+let myInt : Int = (2 + (3 % 4) * 5)
+
+let myInt2 : Int = (((2 + 3) % 4) * 5 )
+
+//Mas Operadores avanzados
+
+// - Operadores bit a bit. Realizar operaciones de tipo NOT, AND, OR, XOR
+//Operadores de desplazamineto : Para desplazar bits
+//Operadores de desbordamiento: Para manejo de errores
+
