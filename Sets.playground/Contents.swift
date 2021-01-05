605391f0fc5d969097354f197321f3bf6984dc15
Eb Peña <eberg.pena@gmail.com>
2021-01-04 22:29:59 -0600

    Se realizo un documento para configurar tipo set

---
+++ Sets.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var mySet = Set<String>()
+let mySet2: Set<String> = ["Eber", "Moure", "32", "Peña"]
+
+//Isercción
+mySet.insert("Eber")
+mySet.insert("Moure")
+mySet.insert("30")
+mySet.insert("Peña")
+print(mySet)
+
+
+//Acceso de los sets
+if mySet.contains("Moure"){
+    print("Existe")
+}else{
+    print("No existe")
+}
+
+
+// Modificación
+
+// Acceso y Modificación
