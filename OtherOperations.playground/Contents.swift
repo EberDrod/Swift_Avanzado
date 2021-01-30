d40195a3b446bc88cfe0455e0ed485379fe5b586
Eber Peña <eberg.pena@gmail.com>
2021-01-29 18:37:12 -0600

    Se han agregado otras operaciones como ejemplo

---
+++ OtherOperations.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// Otras operaciones con colecciones
+
+var myArray = [5,8,1,0,3,9,7,2,4,6]
+
+let myDictionary  = [5:"Cinco",
+                     8:"Ocho",
+                     1:"Uno",
+                     0:"Cero",
+                     3:"Tres",
+                     9:"Nueve",
+                     7:"Siete",
+                     2:"Dos",
+                     4:"Cuatro",
+                     6:"Seis"]
+let mySet: Set = [5,8,1,9,3,9,7,2,4,6]
+
+
+//Count
+print(myArray.count)
+print(myDictionary.count)
+print(mySet.count)
+
+//is Empity
+
+print(myArray.isEmpty)
+print(myDictionary.isEmpty)
+print(mySet.isEmpty)
+
+
+
